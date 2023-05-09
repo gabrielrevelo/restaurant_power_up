322c6b21fbb2d1f3c7d9c4c1930717878327c502
GaRev-Dev <revelo.gabriel.38@gmail.com>
2023-05-09 16:55:01 -0500

    feat: Add input validation for restaurant creation DTO

--- src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
+++ src/main/java/com/pragma/powerup/usermicroservice/adapters/driving/http/dto/request/RestaurantRequestDto.java
@@ -1,5 +1,8 @@
 package com.pragma.powerup.usermicroservice.adapters.driving.http.dto.request;
 
+import com.pragma.powerup.usermicroservice.configuration.Constants;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,19 @@ import lombok.Setter;
 @Getter
 @Setter
 public class RestaurantRequestDto {
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])[a-zA-Z\\d]+$", message = Constants.INVALID_FORMAT_MESSAGE)
     private String name;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String address;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String idOwner;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "^\\+?[0-9]{1,12}$", message = Constants.INVALID_FORMAT_MESSAGE)
     private String phone;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
     private String urlLogo;
+    @NotEmpty(message = Constants.EMPTY_FIELD_MESSAGE)
+    @Pattern(regexp = "\\d*", message = Constants.INVALID_FORMAT_MESSAGE)
     private String nit;
 }
