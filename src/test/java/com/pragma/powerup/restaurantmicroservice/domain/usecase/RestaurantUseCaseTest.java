40524df049c911f18f4e48b5dfe6216a5ed895a9
Gabriel Revelo <revelo.gabriel.38@gmail.com>
2023-06-05 13:13:09 -0500

    test: refactor test whit coverage 100% usecases

--- src/test/java/com/pragma/powerup/restaurantmicroservice/domain/usecase/RestaurantUseCaseTest.java
+++ src/test/java/com/pragma/powerup/restaurantmicroservice/domain/usecase/RestaurantUseCaseTest.java
@@ -1,14 +1,18 @@
 package com.pragma.powerup.restaurantmicroservice.domain.usecase;
 
 import com.pragma.powerup.restaurantmicroservice.domain.exceptions.UserNotOwnerException;
+import com.pragma.powerup.restaurantmicroservice.domain.model.Employee;
 import com.pragma.powerup.restaurantmicroservice.domain.model.Restaurant;
+import com.pragma.powerup.restaurantmicroservice.domain.spi.IEmployeeRestaurantPersistencePort;
 import com.pragma.powerup.restaurantmicroservice.domain.spi.IUserClient;
 import com.pragma.powerup.restaurantmicroservice.domain.spi.IRestaurantPersistencePort;
+import com.pragma.powerup.restaurantmicroservice.domain.util.AuthorizationUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -20,72 +24,84 @@ class RestaurantUseCaseTest {
     @Mock
     private IRestaurantPersistencePort restaurantPersistencePort;
     @Mock
-    private IUserClient restTemplateClient;
+    private IUserClient userClient;
+    @Mock
+    private IEmployeeRestaurantPersistencePort employeeRestaurantPersistencePort;
+    @Mock
+    private AuthorizationUtil authorizationUtil;
 
     private RestaurantUseCase restaurantUseCase;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort, employeeRestaurantPersistencePort, restTemplateClient, userServicePort);
+        restaurantUseCase = new RestaurantUseCase(restaurantPersistencePort, employeeRestaurantPersistencePort, userClient, authorizationUtil);
     }
 
     @Test
-    void saveRestaurant_WithValidOwner_SavesRestaurant() {
-        // Arrange
+    void saveRestaurant_ValidIdOwner() {
         String ownerId = "1";
         Restaurant restaurant = new Restaurant();
         restaurant.setIdOwner(ownerId);
+        when(userClient.getUserRole(ownerId, null)).thenReturn("ROLE_OWNER");
 
-        when(restTemplateClient.getUserRole(ownerId)).thenReturn("ROLE_OWNER");
-
-        // Act
         restaurantUseCase.saveRestaurant(restaurant);
 
-        // Assert
         verify(restaurantPersistencePort).saveRestaurant(restaurant);
         verify(restaurantPersistencePort, times(1)).saveRestaurant(restaurant);
     }
 
     @Test
-    void saveRestaurant_WithInvalidOwner_ThrowsUserNotOwnerException() {
-        // Arrange
+    void saveRestaurant_InvalidIdOwner() {
         String ownerId = "1";
         Restaurant restaurant = new Restaurant();
         restaurant.setIdOwner(ownerId);
+        when(userClient.getUserRole(ownerId, null)).thenReturn("ROLE_ADMIN");
 
-        when(restTemplateClient.getUserRole(ownerId)).thenReturn("ROLE_ADMIN");
-
-        // Act & Assert
         assertThrows(UserNotOwnerException.class, () -> restaurantUseCase.saveRestaurant(restaurant));
         verify(restaurantPersistencePort, never()).saveRestaurant(restaurant);
     }
 
     @Test
-    void testListRestaurants() {
-        // Arrange
+    void listRestaurants() {
         List<Restaurant> allRestaurants = Arrays.asList(
                 new Restaurant("Restaurant A"),
                 new Restaurant("Restaurant C"),
                 new Restaurant("Restaurant B"),
                 new Restaurant("Restaurant D")
         );
-
         List<Restaurant> expected = Arrays.asList(
-                new Restaurant("Restaurant A"),
-                new Restaurant("Restaurant B")
+                new Restaurant("Restaurant C"),
+                new Restaurant("Restaurant D")
         );
-
         int pageSize = 2;
-        int pageNumber = 1;
-
+        int pageNumber = 2;
         when(restaurantPersistencePort.listRestaurants()).thenReturn(allRestaurants);
 
-        // Act
         List<Restaurant> result = restaurantUseCase.listRestaurants(pageSize, pageNumber);
 
-        // Assert
         assertEquals(expected, result);
-        verify(restaurantPersistencePort, times(1)).listRestaurants();
+        verify(restaurantPersistencePort).listRestaurants();
+    }
+
+    @Test
+    void registerEmployee() {
+        Employee employee = new Employee(
+                1L,
+                "Employee",
+                "Example",
+                "1",
+                "1",
+                "1",
+                "1",
+                LocalDate.now().minusYears(20),
+                1L);
+        Long restaurantId = 1L;
+        when(userClient.createEmployee(employee, null)).thenReturn(employee.getId());
+
+        Employee result = restaurantUseCase.registerEmployee(restaurantId, employee);
+
+        assertEquals(employee, result);
+        verify(employeeRestaurantPersistencePort).saveEmployeeRestaurant(employee.getId(), restaurantId);
     }
 }
